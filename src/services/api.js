aa191ab15fa8adb7f66496b02287fb61f9613b9e
Saminu Awwal <awwalsaminu9@gmail.com>
2026-08-23 20:08:38 +0100

    changed the base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://ifrs-api.hdiportal.com/api";
 
 const getHeaders = () => {
   const token = localStorage.getItem("hdi_token");
